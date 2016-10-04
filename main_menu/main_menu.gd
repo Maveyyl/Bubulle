0f8d33ad474e0b3079c77b6efcf1a9bbc44c2e91
Maveyyl <martialveyry@hotmail.fr>
2016-10-04 18:33:27 +0200

    Added main menu scene

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+func _on_button_solo_pressed():
+	pass # replace with function body
+
+func _on_button_multi_pressed():
+	pass # replace with function body
+
+func _on_button_tutorial_pressed():
+	pass # replace with function body
+
+func _on_button_training_pressed():
+	pass # replace with function body
+
+func _on_button_options_pressed():
+	pass # replace with function body
+
+func _on_button_leave_pressed():
+	pass # replace with function body
